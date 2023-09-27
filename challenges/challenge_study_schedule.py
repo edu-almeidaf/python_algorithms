4f62b17ae5ac5a5afa8430c77046a94d17000870
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-09-26 23:05:06 -0300

    feat (REQ01): Created function study_schedule

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,11 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not target_time:
+        return None
+
+    verify_entries = 0
+    for entries, exits in permanence_period:
+        if type(entries) != int or type(exits) != int:
+            return None
+        if entries <= target_time <= exits:
+            verify_entries += 1
+    return verify_entries
