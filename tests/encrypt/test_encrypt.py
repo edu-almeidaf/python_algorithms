48fa38d29246d84e6d3cdbc3db0c3e1316ecc104
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-09-26 23:43:08 -0300

    feat (REQ02): Created tests for encrypt_message function

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("EDUARDO", 30) == "ODRAUDE"
+    assert encrypt_message("EDUARDO", 5) == "RAUDE_OD"
+    assert encrypt_message("EDUARDO", 4) == "ODR_AUDE"
+
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message("ABCDEF", "1")
+
+    with pytest.raises(TypeError, match="tipo inválido para message"):
+        encrypt_message(48, 12)
