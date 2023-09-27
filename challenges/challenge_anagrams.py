1418fdfad04560347cdc12b8477b54bd2177f70c
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-09-27 00:59:58 -0300

    feat (REQ04): Created function challenge_anagrams.py

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,21 @@
+def sort_word(word):
+    if len(word) <= 1:
+        return word
+
+    divider = word[len(word) // 2]
+    left = "".join([letter for letter in word if letter < divider])
+    middle = "".join([letter for letter in word if letter == divider])
+    right = "".join([letter for letter in word if letter > divider])
+
+    return sort_word(left) + middle + sort_word(right)
+
+
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first_string_sorted = sort_word(first_string.lower())
+    second_string_sorted = sort_word(second_string.lower())
+
+    if not first_string or not second_string:
+        return (first_string_sorted, second_string_sorted, False)
+    is_anagram_string = first_string_sorted == second_string_sorted
+
+    return first_string_sorted, second_string_sorted, is_anagram_string
